a99df95dbb72e06f4add62f40a2ae64d0f7f72fd
NetworkManager <93763064+nwkman@users.noreply.github.com>
2022-04-06 11:17:58 +0800

    Add initialize.js

---
+++ initialize.js
@@ -0,0 +1,22 @@
+// initialize all things.
+async function init() {
+    try {
+        await getQuotes();
+        await getErrands();
+        currentQuoteId = Math.floor(Math.random() * quotesList.length);
+        quotes.innerHTML = quotesList[currentQuoteId].text;
+        var updateInterval = setInterval(function() {
+            loadTimer();
+        }, 1000);
+        clock.addEventListener("click", function() {
+            toggleMode();
+            loadTimer();
+        });
+        quotes.addEventListener("click", function() {
+            toggleQuote();
+        })
+    } catch (e) {
+        app.innerHTML = `<div class="center error">Sorry, this timer has been misconfigured. Please contact the owner of this website to fix it.<br>Error: ${e}</div>`;
+    }
+    return;
+}
